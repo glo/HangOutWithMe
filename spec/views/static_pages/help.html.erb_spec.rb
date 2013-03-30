c61eddba6625d8eef9276f064d07af66b39898d5
glo <gloria.lin@gmail.com>
2013-03-30 16:23:31 -0700

    Finish layout and routes

--- spec/views/static_pages/help.html.erb_spec.rb
+++ spec/views/static_pages/help.html.erb_spec.rb
@@ -1,7 +1,5 @@
-<h1>Help</h1>
-<p>
-  Get help on the Ruby on Rails Tutorial at the
-  <a href="http://railstutorial.org/help">Rails Tutorial help page</a>.
-  To get help on this sample app, see the
-  <a href="http://railstutorial.org/book">Rails Tutorial book</a>.
-</p>
+require 'spec_helper'
+
+ describe "static_pages/help.html.erb" do
+   pending "add some examples to (or delete) #{__FILE__}"
+ end
