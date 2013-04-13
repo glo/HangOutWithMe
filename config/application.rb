18824d295e4106e70cd8f7add1a2740941403941
glo <gloria.lin@gmail.com>
2013-04-13 11:03:16 -0700

    Change back config.assets.enabled=ture

--- config/application.rb
+++ config/application.rb
@@ -54,8 +54,7 @@ module HelloWorld
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = false
-    # config.assets.enabled = true
+    config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
